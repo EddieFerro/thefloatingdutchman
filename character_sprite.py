9038d893bf0453e9e74edbe6f9f70f1e8f8eac97
Kayton Fletcher <kaytonfletcher@gmail.com>
2020-06-13 15:42:30 -0400

    added test player sprite with rotation

--- character_sprite.py
+++ character_sprite.py
@@ -1,67 +1,79 @@
 import pygame.sprite
 import math
-from GameSettings import *
-vector = pygame.math.Vector2
+import pygame
+from GameSettings import GREEN, PLAYER_SPEED
+from pygame.math import Vector2
+
 
 class CharacterSprite(pygame.sprite.Sprite):
 
     def __init__(self, spawnx, spawny):
         pygame.sprite.Sprite.__init__(self)
-
-        self.original_image = pygame.Surface((20,50))
-        self.original_image.fill(GREEN)
-        self.image = pygame.Surface((20, 50))  # sets the size of the sprite
-        self.image.fill(GREEN)  # sets the color of the sprite, default green for visibility
-
-        # self.image = pygame.image.load("").convert()  ---- this will be used later to import an image for a sprite
-        self.rect = self.image.get_rect()  # creates the rectangle
-        self.rect.center = (spawnx, spawny)  # sets the spawn point
-        self.pos = vector(spawnx, spawny)
+        self.pos = Vector2(spawnx, spawny)
 
 
 class PlayerSprite(CharacterSprite):
     def __init__(self, spawnx, spawny):
         super().__init__(spawnx, spawny)
-        # self.original_image = pygame.image.load("").convert()
-        # self.original_image = pygame.transform.scale(self.original_image, (100, 100))
-        # self.image = pygame.image.load("").convert()
-        # self.image = pygame.transform.scale(self.image, (100, 100))
+        ss = pygame.image.load("topdown_sample.png").convert()
 
-    def movement(self):
-        self.velx = 0
-        self.vely = 0
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            self.velx = -PLAYER_SPEED
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            self.velx = PLAYER_SPEED
-        if keys[pygame.K_UP] or keys[pygame.K_w]:
-            self.vely = -PLAYER_SPEED
-        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            self.vely =  PLAYER_SPEED
-        self.fix_diag_move()
-        self.vel = (self.velx, self.vely)
+        # exact dimension of player sprite
+        self.rect = pygame.Rect((0, 0, 313, 207))
+        self.image = pygame.Surface(self.rect.size).convert()
 
-    def fix_diag_move(self):
-        if self.velx != 0 and self.vely != 0:
-            self.velx *= 0.7071
-            self.vely *= 0.7071
+        # sets image to a portion of spritesheet (surface)
+        self.image.blit(ss, (0, 0), self.rect)
+
+        # makes player appropriate size
+        self.image = pygame.transform.scale(
+            self.image, (int(313/4), int(207/4)))
+
+        self.original_image = self.image
 
     # simple player movement
     def update(self):
         pygame.event.pump()
-        self.movement()
+        self.calc_movement()
+
+    def calc_movement(self):
+        x = 0
+        y = 0
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            x = -PLAYER_SPEED
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            x = PLAYER_SPEED
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            y = -PLAYER_SPEED
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            y = PLAYER_SPEED
+
+        if x != 0 and y != 0:
+            x *= 0.7071
+            y *= 0.7071
+
+        self.vel = Vector2(x, y)
+        self.pos += self.vel
+        self.calc_rotation()
+
+    def calc_rotation(self):
         mouse_x, mouse_y = pygame.mouse.get_pos()
         rel_x, rel_y = mouse_x - self.pos.x, mouse_y - self.pos.y
-        angle = (180 / math.pi) * -math.atan2(rel_y, rel_x)
+        angle = (180 / math.pi) * -math.atan2(rel_y, rel_x) + 5
         self.image = pygame.transform.rotate(self.original_image, int(angle))
         self.rect = self.image.get_rect(center=self.pos)
-        self.pos += self.vel
         self.rect.center = self.pos
+ 
 
 class EnemySprite(CharacterSprite):
     def __init__(self, spawnx, spawny):
         super().__init__(spawnx, spawny)
+        self.original_image = pygame.Surface((20, 50))
+        self.original_image.fill(GREEN)
+        self.image = self.original_image
+
+        self.rect = self.image.get_rect()  # creates the rectangle
+        self.rect.center = (spawnx, spawny)  # sets the spawn point
 
     # Enemy AI might go in here
     # def update(self):
