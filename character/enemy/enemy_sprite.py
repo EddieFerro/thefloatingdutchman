190838d6411c391bfc0bf4b9a9ee08f6a5aa4a75
Santosh <stirumala@ufl.edu>
2020-06-21 16:39:30 -0400

    Improvements to AI, changes to play again to make testing easier

--- character/enemy/enemy_sprite.py
+++ character/enemy/enemy_sprite.py
@@ -1,6 +1,8 @@
 from pygame import Surface, Vector2
 from pygame.sprite import Group
 
+import pygame
+
 from character.character_sprite import CharacterSprite
 from character.player.player_sprite import PlayerSprite
 from character.enemy.enemy_data import EnemyData
@@ -10,6 +12,9 @@ from game_settings import GREEN
 class EnemySprite(CharacterSprite):
     def __init__(self, enemy_data: EnemyData):
         super().__init__(enemy_data)
+
+
+        self.radius = 50
 
     def _set_original_image(self):
         self._original_image = Surface((20, 50))
@@ -24,10 +29,10 @@ class EnemySprite(CharacterSprite):
             if self.rect.colliderect(player.rect):
                 enemies.remove(self)
             for enemy in enemies:
-                if self.rect.colliderect(enemy.rect) and enemy != self:
+                if pygame.sprite.collide_circle(self, enemy) and enemy != self:
                     direction_vector = Vector2(
                         (self.rect.x - enemy.rect.x),  (self.rect.y - enemy.rect.y))
-                    direction_vector.scale_to_length(self._data.vel * 2)
+                    direction_vector.scale_to_length(self._data.vel*2)
             self.rect.move_ip(direction_vector)
         except ValueError:
             return
