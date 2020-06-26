518fc82b3ef03cd620506ae66b0cf41f00ae135c
Santosh <stirumala@ufl.edu>
2020-06-26 18:52:06 -0400

    added comments to ai

--- character/enemy/enemy_sprite.py
+++ character/enemy/enemy_sprite.py
@@ -21,7 +21,7 @@ class EnemySprite(CharacterSprite):
         self._angle = 0
 
 
-        self.radius = 50
+        self.radius = 100
 
     def _set_original_image(self):
         self._original_image = Surface((20, 50))
@@ -29,33 +29,39 @@ class EnemySprite(CharacterSprite):
 
     # Enemy AI might go in here
     def update(self, player: PlayerSprite, enemies: Group):
+        # Check for nearby enemies, only move in certain case
         for enemy in enemies:
             if pygame.sprite.collide_circle(self, enemy) and enemy != self:
                 distance = math.hypot((enemy.rect.x - self.rect.x), (enemy.rect.y - self.rect.x))
                 if (distance < 400):
-                    direction_vector = Vector2(
+                    target_direction = Vector2(
                         (self.rect.x - enemy.rect.x), (self.rect.y - enemy.rect.y))
-                    direction_vector.scale_to_length(self._data.vel * 1.01)
-                    self.rect.x += direction_vector.x
-                    self.rect.y += direction_vector.y
+                    target_direction.scale_to_length(self._data.vel * 1.01)
+                    self.rect.x += target_direction.x
+                    self.rect.y += target_direction.y
+
+        # Type 2 enemy backs away from player
         distance = math.hypot((player.rect.x - self.rect.x), (player.rect.y - self.rect.y))
         if (distance > 300 and self._data._type2):
             self._data._stopMoving = False
 
-
+        # Enemy moves toward player given that they are either type 1 or sufficiently far enough from player
         if self._data._stopMoving == False:
-            direction_vector = Vector2(
+            target_direction = Vector2(
                 - self.rect.x + player.rect.x, - self.rect.y + player.rect.y)
-            direction_vector.scale_to_length(self._data.vel)
+            target_direction.scale_to_length(self._data.vel)
 
         try:
+            # Update bullets
             self._bullets.update()
 
+            # Delete enemy when it comes into contact with player
             if self.rect.colliderect(player.rect):
                 enemies.remove(self)
 
-
+            # Tyoe 2 enemy specification
             if self._data._type2:
+                # Auto fire towards player
                 t = pygame.time.get_ticks()
                 if (t - self._prev_shot) > 1000:
                     self._prev_shot = t
@@ -64,19 +70,23 @@ class EnemySprite(CharacterSprite):
                     direction = Vector2(
                         1, 0).rotate(self._angle)
                     BulletSprite(BulletData(10, direction, 0, Vector2(self.rect.x, self.rect.y), 25)).add(self._bullets)
+
+                    # Stop moving towards player at a certain distance
                 if pygame.sprite.collide_circle(self, player):
                     self._data._stopMoving = True
                     distance = math.hypot((player.rect.x-self.rect.x),(player.rect.y - self.rect.y))
+                    # Back up until edge of danger zone
                     if(distance < 490):
-                        direction_vector = Vector2(
+                        target_direction = Vector2(
                             (self.rect.x - player.rect.x), (self.rect.y -player.rect.y))
-                        direction_vector.scale_to_length(self._data.vel * 1.1)
-                        self.rect.x += direction_vector.x
-                        self.rect.y += direction_vector.y
+                        target_direction.scale_to_length(self._data.vel * 1.1)
+                        self.rect.x += target_direction.x
+                        self.rect.y += target_direction.y
 
+            # All other cases are given movement data here
             if self._data._stopMoving == False:
-                self.rect.x += direction_vector.x
-                self.rect.y += direction_vector.y
+                self.rect.x += target_direction.x
+                self.rect.y += target_direction.y
 
 
         except ValueError:
