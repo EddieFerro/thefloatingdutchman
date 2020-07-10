0d43c6aaf4a7b9a7b07f4ae587ab23397de4d868
Santosh <stirumala@ufl.edu>
2020-07-09 23:10:21 -0400

    subclass enemies

--- thefloatingdutchman/character/enemy/enemy_data.py
+++ thefloatingdutchman/character/enemy/enemy_data.py
@@ -5,8 +5,9 @@ import random
 class EnemyData(CharacterData):
     """Enemy class for base enemy can be later subclassed to create specific types of enemies """
     # Will make sure to subclass enemy types later on
-    def __init__(self, health, attack_speed, spawn, vel):
+    def __init__(self, health, attack_speed, spawn, vel, level):
         super().__init__(health, attack_speed, spawn, vel)
-        self._type2 = random.choices([True,False], weights=[0.2, 0.8], k = 1)[0]
         self._stopMoving = False
+
+
 
