52620c7f69f6c5397a8df217b32c48a9bf09e87d
Kayton Fletcher <kaytonfletcher@gmail.com>
2020-07-15 18:14:08 -0400

    random map completed

--- thefloatingdutchman/level/room/room_manager.py
+++ thefloatingdutchman/level/room/room_manager.py
@@ -1,5 +1,6 @@
 from typing import List
-from random import randint, sample
+from random import randint, choices
+from ordered_set import OrderedSet
 
 import networkx as nx
 from pygame import Surface
@@ -19,71 +20,122 @@ class RoomManager(Manager):
         self._room_graph = nx.DiGraph()
         self._current_room_id = 0
 
-    def _generate_room_graph(self):
+    def _generate_default_graph(self):
         self._number_of_rooms = 11
+        self.rooms_per_col.append(1)  # 0
+        self.rooms_per_col.append(2)  # 1, 2
+        self.rooms_per_col.append(4)  # 3, 4, 5, 6
+        self.rooms_per_col.append(3)  # 7, 8, 9
+        self.rooms_per_col.append(1)  # 10
+        self._room_graph.add_edges_from(
+            [(0, 1), (0, 2), (1, 3), (1, 4), (1, 5), (2, 5),
+             (2, 6), (3, 7), (4, 7), (5, 8), (6, 9),
+             (7, 10), (8, 10), (9, 10)
+             ])
+
+    def _generate_room_graph(self, num_rooms, max_per_col):
+        self._number_of_rooms = num_rooms
 
         for i in range(0, self._number_of_rooms):
             self._rooms.append(Room())
 
-        # room_cols = 5 == len(self.rooms_per_col)
         # first and last col should always have 1
-        # self.rooms_per_col.append(1)  # 0
-        # self.rooms_per_col.append(2)  # 1, 2
-        # self.rooms_per_col.append(4)  # 3, 4, 5, 6
-        # self.rooms_per_col.append(3)  # 7, 8, 9
-        # self.rooms_per_col.append(1)  # 10
-
         self.rooms_per_col.append(1)  # 0
 
-        max_per_col = 4
         rooms_left = self._number_of_rooms - 2
-        
-        while rooms_left > max_per_col+1:
+
+        while rooms_left >= max_per_col+1:
             temp = randint(2, max_per_col)
             self._rooms_per_col.append(temp)
             rooms_left -= temp
 
+        if rooms_left == 1:
+            rooms_left += 1
+            self._number_of_rooms += 1
+
         self._rooms_per_col.append(rooms_left)
         self.rooms_per_col.append(1)  # 10
 
-        print("ROOMS PER COL: ", self._rooms_per_col)
-
         first_in_col = 0
         for i, room_count in enumerate(self.rooms_per_col):
-            if i == len(self.rooms_per_col) - 1:
-                break
 
             next_room_count = self.rooms_per_col[i+1]
-            last_in_col = first_in_col+room_count-1
+            first_in_next_col = first_in_col+room_count
+            last_in_col = first_in_next_col-1
+            next_col_range = range(
+                first_in_next_col, first_in_next_col + next_room_count)
 
-            self._room_graph.add_edge(first_in_col, last_in_col+1)
+            if i == len(self.rooms_per_col) - 2:
+                for _id in range(first_in_col, last_in_col+1):
+                    self._room_graph.add_edge(_id, first_in_next_col)
+
+                break
+
             if room_count > 1:
-                self._room_graph.add_edge(last_in_col, last_in_col+next_room_count)
 
-                
-                if room_count > 2 and room_count-2 <= next_room_count:
-                    print("ROOM COUNT: " + str(room_count))
-                    print("Next Room Count: " + str(next_room_count))
-                    print('last in column: ' + str(last_in_col))
-                    print("end of range: " + str(last_in_col+1+next_room_count))
-            
-                    paths = sample(range(last_in_col+1, last_in_col+1+next_room_count), room_count-2)
-                    paths.sort()
-                    print(paths)
-                    for index, _id in enumerate(paths):
-                        self._room_graph.add_edge(first_in_col+1+index, _id)
-            
-            
+                self._room_graph.add_edge(first_in_col, first_in_next_col)
+
+                self._room_graph.add_edge(
+                    last_in_col, last_in_col+next_room_count)
+
+                chosen_ids = choices(next_col_range, k=room_count-2)
+                chosen_ids.sort()
+
+                # picks inside nodes from current col to join to next col
+                for index, _id in enumerate(chosen_ids):
+                    self._room_graph.add_edge(first_in_col+1+index, _id)
+
+                chosen_ids.insert(0, first_in_next_col)
+                chosen_ids.append(first_in_next_col + next_room_count - 1)
+
+                inner_next_col_ids = set(next_col_range)
+
+                # removes first and last from next column set
+                inner_next_col_ids.remove(first_in_next_col)
+
+                if next_room_count != 1:
+                    inner_next_col_ids.remove(
+                        first_in_next_col + next_room_count - 1)
+
+                not_sel_list = list(inner_next_col_ids - set(chosen_ids))
+                not_sel_list.sort(reverse=True)
+                not_selected = OrderedSet(not_sel_list)
+
+                # while we have nodes not selected
+                while not_selected:
+                    _id = not_selected.pop()
+                    j = _id - 1
+                    k = _id
+
+                    node_set = OrderedSet([_id])
+
+                    j_index = [i for i, chosen_id in enumerate(
+                        chosen_ids) if chosen_id == j][-1]
+
+                    node_j_id = j_index + first_in_col
+
+                    while k not in chosen_ids:
+                        k += 1
+
+                        if k not in chosen_ids:
+                            node_set.add(k)
+                            not_selected.pop()
+
+                    k_index = chosen_ids.index(k)
+                    node_k_id = k_index + first_in_col
+
+                    choosy_listy = choices(
+                        range(node_j_id, node_k_id+1), k=len(node_set))
+                    choosy_listy.sort()
+
+                    for nodey_boy, nodey_girl in zip(choosy_listy, node_set):
+                        self._room_graph.add_edge(nodey_boy, nodey_girl)
+
+            else:
+                for _id in next_col_range:
+                    self._room_graph.add_edge(first_in_col, _id)
+
             first_in_col += room_count
-
-        
-        print(self._room_graph)
-
-        # self._room_graph.add_edges_from(
-        #     [(0, 1), (0, 2), (1, 3), (1, 4), (1, 5), (2, 5),
-        #      (2, 6), (3, 7), (4, 7), (5, 8), (6, 9),
-        #      (7, 10), (8, 10), (9, 10)
-        #      ])
 
     def spawn(self, level: int):
         self._rooms = []
@@ -91,7 +143,7 @@ class RoomManager(Manager):
         self._current_room_id = 0
 
         self._room_graph.clear()
-        self._generate_room_graph()
+        self._generate_room_graph(26, 5)
 
         for i, room in enumerate(self._rooms):
             room.spawn(level, i)
