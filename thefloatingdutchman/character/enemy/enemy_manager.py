ae654b00da628eb9da457911e39de7f55ca0dd0f
Santosh <stirumala@ufl.edu>
2020-07-25 14:47:15 -0400

    tweak 2

--- thefloatingdutchman/character/enemy/enemy_manager.py
+++ thefloatingdutchman/character/enemy/enemy_manager.py
@@ -56,7 +56,7 @@ class EnemyManager(Manager):
                             # random.randint(5, 15) + random.randint(0, level*2),
                             1500,
                             Vector2(rand_pos_x, rand_pos_y),
-                            5 + (level * 3)
+                            8 + (level * 3)
                         )
                     )
                 )
